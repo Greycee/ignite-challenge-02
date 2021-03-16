7373572f8a3f4e07ffd52e3d8fa3925ca929fc7d
Greyce Riquinho <33056762+Greycee@users.noreply.github.com>
2021-03-16 13:36:29 -0300

    First commit

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,27 @@
+import { Button } from './Button';
+import { GenreResponseProps } from '../interfaces'
+
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  handleClickButton: (id: number) => void;
+  selectedGenreId: number;
+}
+
+export function SideBar({genres, handleClickButton, selectedGenreId}: SideBarProps) {
+  return(
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              id={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+      </nav>
+  )
+}
